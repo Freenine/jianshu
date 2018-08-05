eb393eac2db2e9d0e1afb9abeff9d828a4ea27ab
wangweijin <64534264@qq.com>
2018-08-05 13:04:24 +0800

    '修复BUG'

--- apps/PyJianShu/views.py
+++ apps/PyJianShu/views.py
@@ -20,7 +20,7 @@ def articleCount(request):
         cursor.execute("select u.name, u.home_page,count(a2.id) as article_count "
                        "from PyJianShu_user as u "
                        "left join (select a.user_id, a.id from PyJianShu_article as a where a.newstime >= %s and a.newstime <= %s) "
-                       "as a2 on u.id=a2.user_id group by u.id order by article_count desc",[time_start, time_end])
+                       "as a2 on u.id=a2.user_id group by u.id order by article_count desc, u.name",[time_start, time_end])
 
         data = dictfetchall(cursor)
 
@@ -36,7 +36,7 @@ def commentCount(request):
         cursor.execute("select u.name, u.home_page,count(a2.id) as comment_count "
                        "from PyJianShu_user as u "
                        "left join (select a.user_id, a.id from PyJianShu_comment as a where a.newstime >= %s and a.newstime <= %s) "
-                       "as a2 on u.id=a2.user_id group by u.id order by comment_count desc", [time_start, time_end])
+                       "as a2 on u.id=a2.user_id group by u.id order by comment_count desc, u.name", [time_start, time_end])
 
         data = dictfetchall(cursor)
 
@@ -52,7 +52,7 @@ def likeCount(request):
         cursor.execute("select u.name, u.home_page,count(a2.id) as like_count "
                        "from PyJianShu_user as u "
                        "left join (select a.user_id, a.id from PyJianShu_like as a where a.newstime >= %s and a.newstime <= %s) "
-                       "as a2 on u.id=a2.user_id group by u.id order by like_count desc", [time_start, time_end])
+                       "as a2 on u.id=a2.user_id group by u.id order by like_count desc, u.name", [time_start, time_end])
 
         data = dictfetchall(cursor)
 
